ad07cdf97ac6cdbc032cf6a10616b8aa2ac0e5d5
ParkChan <hydrarkk@naver.com>
2020-03-13 01:12:53 +0900

    Initial commit

---
+++ app/src/main/java/com/laonsports/kids/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.laonsports.kids
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
